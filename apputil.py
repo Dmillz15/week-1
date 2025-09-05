ef91908ef75205f45bf5350c67b5ab5b9bc1b8b2
Dmillz15 <160161559+Dmillz15@users.noreply.github.com>
2025-09-05 16:44:21 +0000

    apputil file

--- apputil.py
+++ apputil.py
@@ -1,3 +1,34 @@
 
 
 # add code below ...
+# Exercise 1
+import string
+
+def palindrome(word):
+    # Remove spaces and punctuation, and convert to lowercase
+    cleaned = ''.join(char.lower() for char in word if char.isalnum())
+    return cleaned == cleaned[::-1]
+
+print(palindrome('racecar'))                   
+print(palindrome('Nurses Run'))                
+print(palindrome('Sit on a potato pan, Otis.'))
+
+
+
+#Exercise 2
+def parentheses(sequence):
+    stack = []
+    mapping = {')': '('}
+    for char in sequence:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack or stack.pop() != mapping[char]:
+                return False
+    return stack == []
+
+print(parentheses('((blah)()()())'))
+print(parentheses('(((())blee))'))
+print(parentheses('(()hello((())()))'))
+print(parentheses('((((((())'))
+print(parentheses('()))'))
